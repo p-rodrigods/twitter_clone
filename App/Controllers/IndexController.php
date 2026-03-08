653830e78722d809bba6df8f456afaed12b56077
p.rodrigods <prds.tj@gmail.com>
2026-03-07 22:03:25 -0300

    resolvendo conflitos

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers;
+
+//os recursos do miniframework
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class IndexController extends Action {
+
+	public function index() {
+
+		$this->render('index');
+	}
+
+	public function inscreverse() {
+
+		$this->view->errorCadastro = false;
+
+		$this->render('inscreverse');
+	}
+
+	public function registrar(){
+		// receber os dados do formulário
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+		
+		if($usuario->validarCadastro()){
+			
+			if(count($usuario->getUsuarioPorEmail()) == 0){
+				// usuario já existe
+				$usuario->salvar();
+
+				return	$this->render('cadastro');
+				
+			}
+			
+		}
+		$this->view->usuario = array (
+			'nome' => $_POST['nome'],
+			'email' => $_POST['email'],
+			'senha' => $_POST['senha']
+		);
+
+		$this->view->errorCadastro = true;
+		
+		return $this->render('inscreverse');
+		
+	}
+}
